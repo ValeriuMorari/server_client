7db55ee3805919548e1579f083e822aa18eddeed
U-CW01\uidq8132 <uidq8132@IALR670W.cw01.contiwan.com>
2022-07-11 12:01:30 +0300

    initial commit

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+import time 
+
+PORT = 12341
+time.sleep(10)
+IP = socket.gethostbyname('server')
+# IP = 'localhost'
+s = socket.socket()
+s.connect((IP, PORT))
+s.send("something".encode())
+time.sleep(1)
+s.send("alt mesaj".encode())
---
+++ server.py
@@ -0,0 +1,31 @@
+import socket
+import time 
+
+HOST = socket.gethostbyname('server')
+# HOST = 'localhost'
+PORT = 12341
+
+
+print("Starting up...")
+t1 = time.perf_counter()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    while True:
+        if time.perf_counter() - t1 > 10:
+            s.close()
+            print("Timeout reached")
+            break
+        print("Listen...")
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}...")
+            while True:
+                try:
+                    data = conn.recv(1024)
+                except ConnectionResetError:
+                    print("An existing connection was forcibly closed by the remote host")
+                    break
+                if not data:
+                    break
+                print(data.decode())
